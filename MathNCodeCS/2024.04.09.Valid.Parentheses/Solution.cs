4a240c26bd45c2cf3461b6f17cbc73dfbacbec61
Soonil Hong <s.hong.5181@gmail.com>
2024-04-06 00:25:18 -0400

    [LeetCode 20 Valid Parentheses] ready

---
+++ MathNCodeCS/2024.04.09.Valid.Parentheses/Solution.cs
@@ -0,0 +1,12 @@
+namespace MathNCodeCS._2024._04._09.Valid.Parentheses;
+
+// 20 Valid Parentheses
+// https://leetcode.com/problems/valid-parentheses/
+
+public class Solution
+{
+    public bool IsValid(string s)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ MathNCodeCS/2024.04.09.Valid.Parentheses/Test.cs
@@ -0,0 +1,25 @@
+using NUnit.Framework;
+
+namespace MathNCodeCS._2024._04._09.Valid.Parentheses;
+
+public class Test
+{
+    private Solution _solution = null!;
+
+    [OneTimeSetUp]
+    public void SetUp()
+    {
+        _solution = new Solution();
+    }
+
+    [Test]
+    [TestCase("()", true)]
+    [TestCase("()[]{}", true)]
+    [TestCase("(]", false)]
+    [TestCase("]", false)]
+    [TestCase("[", false)]
+    public void AssertThis(string s, bool expected)
+    {
+        Assert.That(_solution.IsValid(s), Is.EqualTo(expected));
+    }
+}
