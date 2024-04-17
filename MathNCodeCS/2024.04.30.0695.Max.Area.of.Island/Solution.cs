9e4f91dba6015341a56a4bcf5449aea69f97c351
Soonil Hong <s.hong.5181@gmail.com>
2024-04-17 09:35:03 -0400

    [LeetCode 695 Max Area of Island] ready

---
+++ MathNCodeCS/2024.04.30.0695.Max.Area.of.Island/Solution.cs
@@ -0,0 +1,12 @@
+namespace MathNCodeCS._2024._04._30._0695.Max.Area.of.Island;
+
+// 695 Max Area of Island
+// https://leetcode.com/problems/max-area-of-island/
+
+public class Solution
+{
+    public int MaxAreaOfIsland(int[][] grid)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ MathNCodeCS/2024.04.30.0695.Max.Area.of.Island/Test.cs
@@ -0,0 +1,61 @@
+using NUnit.Framework;
+
+namespace MathNCodeCS._2024._04._30._0695.Max.Area.of.Island;
+
+public class Test
+{
+    private Solution _solution = null!;
+
+    private static IEnumerable<TestCaseData> TestCases
+    {
+        get
+        {
+            yield return new TestCaseData(
+                new int[][]
+                {
+                    [1, 1, 1, 1, 0],
+                    [1, 1, 0, 1, 0],
+                    [1, 1, 0, 0, 0],
+                    [0, 0, 0, 0, 0],
+                },
+                9);
+
+            yield return new TestCaseData(
+                new int[][]
+                {
+                    [1, 1, 0, 0, 0],
+                    [1, 1, 0, 0, 0],
+                    [0, 0, 1, 0, 0],
+                    [0, 0, 0, 1, 1],
+                },
+                4);
+
+            yield return new TestCaseData(
+                new int[][]
+                {
+                    [0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+                    [0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0],
+                    [0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+                },
+                6);
+        }
+    }
+
+    [OneTimeSetUp]
+    public void SetUp()
+    {
+        _solution = new Solution();
+    }
+
+    [Test]
+    [TestCaseSource(nameof(TestCases))]
+    public void AssertThis(int[][] grid, int expected)
+    {
+        Assert.That(_solution.MaxAreaOfIsland(grid), Is.EqualTo(expected));
+    }
+}
