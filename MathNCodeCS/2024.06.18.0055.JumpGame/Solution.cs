3a53bc93d193816f1841e140962e87d87ea91573
Soonil Hong <s.hong.5181@gmail.com>
2024-06-18 19:55:56 -0400

    [LeetCode 55 Jump Game] ready

---
+++ MathNCodeCS/2024.06.18.0055.JumpGame/Solution.cs
@@ -0,0 +1,12 @@
+namespace MathNCodeCS._2024._06._18._0055.JumpGame;
+
+// 55 Jump Game
+// https://leetcode.com/problems/jump-game
+
+public class Solution
+{
+    public bool CanJump(int[] nums)
+    {
+        throw new NotImplementedException();
+    }
+}
